48255a3861555a6a05faac5ed96278124df7082b
Ryan <franciscoryan012@gmail.com>
2024-03-14 17:52:06 -0300

    commit para corrigir dps

--- components/Body.js
+++ components/Body.js
@@ -1,51 +1,47 @@
 import {View, StyleSheet, FlatList} from 'react-native'
 import H1 from './ui/H1'
 import CardUser from './CardUser'
+import CardProduct from './CardProduct';
+import { useEffect, useState } from 'react';
 
-const users = [
-  {
-    id: 1,
-    name: "Renan Cavichi",
-    email: "renancavichi@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
-  },
-  {
-    id: 2,
-    name: "Maria",
-    email: "maria@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
-  },
-  {
-    id: 3,
-    name: "Manuel",
-    email: "manuel@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
-  },
-  {
-    id: 4,
-    name: "Camila",
-    email: "camila@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
-  },
-  {
-    id: 5,
-    name: "Renan Cavichi",
-    email: "renancavichi@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
-  },
-  {
-    id: 6,
-    name: "Maria",
-    email: "maria@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
-  },
-]
+
 
 const Body = () => {
+  const [users, setUsers] = useState ([])
+  const [products, setProducts] = useState ([])
+
+  const getUsers = async () => {
+    try{
+      const result = await fetch ('https://backendaulaapi.onrender.com')
+      const data = await result.json()
+      console.log(data.success)
+      setUsers(data.users)
+    } catch (error){
+      console.log(error.message)
+    }
+  
+
+  const getProducts = async () => {
+    try{
+      const result = await fetch ('https://backendaulaapi.onrender.com')
+      const data = await result.json()
+      console.log(data.success)
+      setProducts(data.products)
+    } catch (error){
+      console.log(error.message)
+    }
+  }
+}
+  useEffect(() =>{
+    getUsers(),
+    getProducts()
+  },[])
+  
   return (
     <View style={styles.body}>
         <H1 style={styles.usuariosH1}>Usuários</H1>
         <View style={styles.listUser}>
+          {users.length ?
             <FlatList
               data={users}
               renderItem={({item}) => <CardUser user={item} />}
@@ -53,9 +49,12 @@ const Body = () => {
               horizontal={true}
             />
         </View>
+        <FlatList
+          data={products}
+          renderItem={({})}
     </View>
   )
-}
+  }
 
 const styles = StyleSheet.create({
     body: {
